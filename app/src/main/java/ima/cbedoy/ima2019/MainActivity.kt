16b7652941b0107820ede45c62b5ec1171fe903b
Carlos Bedoy <carlos.bedoy@gmail.com>
2019-01-11 22:38:01 -0600

    Added main activity

---
+++ app/src/main/java/ima/cbedoy/ima2019/MainActivity.kt
@@ -0,0 +1,68 @@
+package ima.cbedoy.ima2019
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class MainActivity : AppCompatActivity() {
+
+    var presenter: MainPresenter? = null
+    var adapter: VideoAdapter = VideoAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.googleapis.com/youtube/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+
+        val service = retrofit.create<Service>(Service::class.java)
+
+        presenter = MainPresenter()
+        val interactor = MainInteractor()
+
+        presenter?.interactor = interactor
+        presenter?.viewController = this
+        interactor.presenter = presenter
+        interactor.service = service
+
+        main_activity_recycler_view.layoutManager = LinearLayoutManager(this)
+        main_activity_recycler_view.setHasFixedSize(true)
+        main_activity_recycler_view.adapter = adapter
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        presenter?.loadVideos()
+    }
+
+    fun onLoadedVideos(videos: ArrayList<String>) {
+        videos.forEach { it ->
+            it.toString()
+        }
+    }
+
+    fun onLoadedVideo(videoItem: VideoItem) {
+        adapter.dataModel.add(videoItem)
+
+        runOnUiThread {
+            adapter.dataModel.sortedWith(compareByDescending<VideoItem> { it.like }
+                .thenByDescending { it.like })
+
+
+            adapter.notifyDataSetChanged()
+        }
+
+    }
+}
